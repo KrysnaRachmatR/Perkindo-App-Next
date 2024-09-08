37624b8375fdf42b74b757f4df646fb588151a21
KrysnaRachmatR <krysnarachmat1@gmail.com>
2024-09-08 19:33:28 +0700

    Memperbarui komponen Beranda untuk menyertakan logika transisi gambar latar belakang yang baru.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,68 @@
-import Image from "next/image";
+"use client"; // Menandakan bahwa ini adalah Client Component
+
+import { useState, useEffect } from "react";
 import Navbar from "@/components/organisms/navbar";
 
+const backgroundImages = [
+  "/images/konstruksi.jpg",
+  "/images/konstruksi2.jpg",
+  "/images/konstruksi3.jpg",
+];
+
 export default function Home() {
+  const [currentImage, setCurrentImage] = useState(backgroundImages[0]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage(prevImage => {
+        const currentIndex = backgroundImages.indexOf(prevImage);
+        const nextIndex = (currentIndex + 1) % backgroundImages.length;
+        return backgroundImages[nextIndex];
+      });
+    }, 3000); // Ganti gambar setiap 5 detik
+
+    return () => clearInterval(interval); // Hapus interval saat komponen di-unmount
+  }, []);
+
   return (
-    <div>
+    <>
       <Navbar />
-      <main className="container mx-auto p-4">
-        <h1 className="text-4x1 font-bold">Welcome to My Website</h1>
-        <p className="mt-4">This is the home page.</p>
-      </main>
-    </div>
+      <div
+        style={{
+          backgroundImage: `url(${currentImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "100vh", // Memastikan background menutupi seluruh layar
+          width: "100vw",
+          position: 'absolute',
+          top: 0,
+          left:0,
+          opacity:1,
+          transition: 'background-image 1s ease-in-out', // Transisi halus saat gambar berganti
+        }}
+      >
+        <div className="flex items-center justify-center h-full">
+          <h1 className="text-white text-4xl font-bold">Selamat Datang di PERKINDO</h1>
+        </div>
+      </div>
+      <div className="bg-white p-8">
+        <div className="max-w-7xl mx-auto">
+          <h2 className="text-3xl font-bold mb-4">Konten Utama</h2>
+          <p className="text-lg mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. 
+          </p>
+          <p className="text-lg mb-4">
+            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor suscipit, eget rhoncus libero bibendum. Nulla condimentum turpis sed augue feugiat, id scelerisque mauris venenatis.
+          </p>
+          <p className="text-lg mb-4">
+            Ut tristique purus nec augue fermentum, sed tincidunt felis lacinia. Nunc id ultrices libero. Integer ut erat magna. Sed tempus massa et libero gravida, non gravida turpis auctor. Nam eget mauris nec augue sodales auctor ut non ligula. Aliquam erat volutpat. Phasellus sit amet ipsum arcu. In nec ipsum mauris. Aliquam erat volutpat.
+          </p>
+          <p className="text-lg mb-4">
+            Mauris facilisis, urna at fermentum sagittis, orci sapien bibendum risus, nec maximus metus magna vel nisl. Vestibulum euismod, purus nec congue luctus, metus odio pharetra libero, ut condimentum arcu justo sit amet metus. Fusce viverra, orci sit amet sollicitudin sodales, purus odio tristique dolor, nec scelerisque dolor erat nec urna. Vivamus vulputate lorem vitae magna vestibulum, sed pellentesque justo pharetra.
+          </p>
+          <div className="h-96"></div> {/* Tambahkan ruang kosong untuk memastikan scroll */}
+        </div>
+      </div>
+    </>
   );
-};
+}
--- src/components/organisms/navbar.tsx
+++ src/components/organisms/navbar.tsx
@@ -1,179 +1,84 @@
 "use client";
 
-import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import styles from "@/components/organisms/navbar.module.css";
 
 const Navbar = () => {
-  const [isClick, setisClick] = useState(false);
+  const [scroll, setScroll] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleNavbar = (): void => {
-    setisClick(!isClick);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <>
-      <nav className="bg-blue-600">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <a href="/" className="flex items-center">
-                <img src="/images/logo.png" alt="logo-perkindo" className="h-14 w-14"/>
-                <span className="text-white font-bold text-2xl ml-4">PERKINDO
-                  <br/> <p className="text-sm">Persatuan Konsultan Indonesia</p>
-                </span>
-                </a>
-
-              </div>
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-4 flex items-center space-x-4">
-                <a
-                  href="/"
-                  className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Beranda
-                </a>
-                <a
-                  href="/"
-                  className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Profil
-                </a>
-                <a
-                  href="/"
-                  className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Galeri
-                </a>
-                <a
-                  href="/"
-                  className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Berita
-                </a>
-                <a
-                  href="/"
-                  className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Layanan
-                </a>
-                <a
-                  href="/"
-                  className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Anggota
-                </a>
-                <a
-                  href="/"
-                  className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Kontak
-                </a>
-                <a
-                  href="/"
-                  className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Login
-                </a>
-              </div>
-            </div>
-            <div className="md:hidden flex items-center">
-              <button
-                className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                onClick={toggleNavbar}
-              >
-                {isClick ? (
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
+    <nav className={`${styles.navbar} ${scroll ? styles.navbarColored : styles.navbarTransparent}`}>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <a href="/" className="flex items-center">
+              <img src="/images/logo.png" alt="logo" className="h-14 w-14" />
+              <span className="text-white font-bold text-2xl ml-4">
+                PERKINDO
+                <br />
+                <p className="text-sm">Persatuan Konsultan Indonesia</p>
+              </span>
+            </a>
+          </div>
+          <div className="hidden md:flex items-center space-x-4">
+            <a href="/" className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2">Beranda</a>
+            <a href="/" className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2">Profil</a>
+            <a href="/" className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2">Galeri</a>
+            <a href="/" className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2">Berita</a>
+            <a href="/" className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2">Layanan</a>
+            <a href="/" className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2">Anggota</a>
+            <a href="/" className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2">Kontak</a>
+            <a href="/" className="text-white hover:bg-white hover:text-black rounded-lg px-3 py-2">Login</a>
+          </div>
+          <div className="md:hidden flex items-center">
+            <button
+              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              onClick={toggleMenu}
+            >
+              {isMenuOpen ? (
+                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              ) : (
+                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+              )}
+            </button>
           </div>
         </div>
-        {isClick && (
+        {isMenuOpen && (
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-               <a
-                  href="/"
-                  className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Beranda
-                </a>
-                <a
-                  href="/"
-                  className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Profil
-                </a>
-                <a
-                  href="/"
-                  className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Galeri
-                </a>
-                <a
-                  href="/"
-                  className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Berita
-                </a>
-                <a
-                  href="/"
-                  className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Layanan
-                </a>
-                <a
-                  href="/"
-                  className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Anggota
-                </a>
-                <a
-                  href="/"
-                  className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Kontak
-                </a>
-                <a
-                  href="/"
-                  className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
-                >
-                  Login
-                </a>
+              <a href="/" className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2">Beranda</a>
+              <a href="/" className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2">Profil</a>
+              <a href="/" className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2">Galeri</a>
+              <a href="/" className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2">Berita</a>
+              <a href="/" className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2">Layanan</a>
+              <a href="/" className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2">Anggota</a>
+              <a href="/" className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2">Kontak</a>
+              <a href="/" className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2">Login</a>
             </div>
           </div>
         )}
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
