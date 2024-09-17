c699a90959e1745b7465e9798251bb0df7cd78c3
Farhan Dwi Pramana <farhanndwipramana@gmail.com>
2024-09-17 22:54:43 +0700

    feat(navbar): add submenu to menu 'Berita'

--- src/components/organisms/navbar.tsx
+++ src/components/organisms/navbar.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { usePathname } from "next/navigation"; // Import usePathname untuk mendeteksi halaman aktif
+import { usePathname } from "next/navigation";
 import styles from "@/components/organisms/navbar.module.css";
 
 const Navbar = () => {
   const [scroll, setScroll] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const pathname = usePathname(); // Dapatkan path saat ini
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const pathname = usePathname();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,18 +25,27 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   const getLinkClassName = (path: string) =>
     `text-white hover:bg-white hover:text-black rounded-lg px-3 py-2 ${
       pathname === path ? "border-b-2 border-yellow-500" : ""
     }`;
 
-  
   const isContactPage = pathname === "/contact";
+  const isLayananPage = pathname === "/layanan";
 
   return (
     <nav
-    className={`${styles.navbar} ${scroll ? styles.navbarColored : styles.navbarTransparent}`}
-    style={isContactPage ? { backgroundColor: "#161D6F" } : {}}>
+      className={`${styles.navbar} ${
+        scroll ? styles.navbarColored : styles.navbarTransparent
+      }`}
+      style={
+        isContactPage || isLayananPage ? { backgroundColor: "#161D6F" } : {}
+      }
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
@@ -49,8 +59,6 @@ const Navbar = () => {
             </a>
           </div>
           <div className="hidden md:flex items-center space-x-2">
-            {" "}
-            {/* Adjusted spacing */}
             <a href="/" className={getLinkClassName("/")}>
               Beranda
             </a>
@@ -60,10 +68,39 @@ const Navbar = () => {
             <a href="/galeri" className={getLinkClassName("/galeri")}>
               Galeri
             </a>
-            <a href="/news" className={getLinkClassName("/news")}>
-              Berita
-            </a>
-            <a href="/services" className={getLinkClassName("/services")}>
+            {/* Berita Dropdown */}
+            <div className="relative">
+              <button
+                onClick={toggleDropdown}
+                className="text-white flex hover:bg-white hover:text-black rounded-lg px-3 py-2  items-center"
+              >
+                <span>Berita</span>
+                <span className="ml-2 text-xs">&#9660;</span>
+              </button>
+              {isDropdownOpen && (
+                <div className="absolute left-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+                  <a
+                    href="/news"
+                    className="block px-4 py-2 text-black hover:bg-gray-200"
+                  >
+                    Berita
+                  </a>
+                  <a
+                    href="/news/current"
+                    className="block px-4 py-2 text-black hover:bg-gray-200"
+                  >
+                    Agenda Berlangsung
+                  </a>
+                  <a
+                    href="/news/past"
+                    className="block px-4 py-2 text-black hover:bg-gray-200"
+                  >
+                    Agenda Sudah Berlangsung
+                  </a>
+                </div>
+              )}
+            </div>
+            <a href="/layanan" className={getLinkClassName("/layanan")}>
               Layanan
             </a>
             <a href="/members" className={getLinkClassName("/members")}>
@@ -74,8 +111,7 @@ const Navbar = () => {
             </a>
             <button className="bg-yellow-500 text-[#161D6F] px-4 py-2 rounded">
               Login
-            </button>{" "}
-            {/* Replaced login link with button */}
+            </button>
           </div>
           <div className="md:hidden flex items-center">
             <button
@@ -116,9 +152,10 @@ const Navbar = () => {
             </button>
           </div>
         </div>
+
         {isMenuOpen && (
           <div className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 bg-[#161D6F]">
               <a
                 href="/"
                 className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
@@ -137,14 +174,41 @@ const Navbar = () => {
               >
                 Galeri
               </a>
+              {/* Dropdown Berita di Mobile */}
+              <div className="relative">
+                <button
+                  onClick={toggleDropdown}
+                  className="text-white flex hover:bg-white hover:text-black rounded-lg px-3 py-2  items-center"
+                >
+                  <span>Berita</span>
+                  <span className="ml-2 text-xs">&#9660;</span>
+                </button>
+                {isDropdownOpen && (
+                  <div className="space-y-1 ml-3 bg-white rounded-md">
+                    <a
+                      href="/news"
+                      className="block pl-5  text-black hover:bg-[#161D6F] hover:text-white px-3 py-2 rounded-t-md"
+                    >
+                      Berita
+                    </a>
+                    <a
+                      href="/news/current"
+                      className="block pl-5 text-black hover:bg-[#161D6F] hover:text-white px-3 py-2"
+                    >
+                      Agenda Berlangsung
+                    </a>
+                    <a
+                      href="/news/past"
+                      className="block pl-5 text-black hover:bg-[#161D6F] hover:text-white px-3 py-2 rounded-b-md"
+                    >
+                      Agenda Sudah Berlangsung
+                    </a>
+                  </div>
+                )}
+              </div>
+
               <a
-                href="/news"
-                className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
-              >
-                Berita
-              </a>
-              <a
-                href="/services"
+                href="/layanan"
                 className="text-white block hover:bg-white hover:text-black rounded-lg px-3 py-2"
               >
                 Layanan
@@ -163,7 +227,7 @@ const Navbar = () => {
               </a>
               <button className="bg-yellow-500 text-[#161D6F] block px-4 py-2 rounded">
                 Login
-              </button>{" "}
+              </button>
             </div>
           </div>
         )}
