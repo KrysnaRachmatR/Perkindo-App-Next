8e7e8a09f26ce19962cadaf1751ab7f2c15fce7f
Farhan Dwi Pramana <farhanndwipramana@gmail.com>
2024-11-28 23:20:54 +0700

    Feat: Melindungi halaman users

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,9 +1,33 @@
+"use client";
 import KtaLayout from "@/components/organisms/users/KTA/page";
 import DefaultLayoutUsers from "@/components/templates/DefaultUsersTemplate";
-
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 export default function Home() {
   const userData = { name: "John Doe", hasKTA: true };
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login");
+    } else {
+      setLoading(false);
+    }
+  }, [router]);
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center space-x-2">
+          <div className="w-16 h-16 border-4 border-t-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
+          <span className="text-xl font-semibold text-gray-600">
+            Loading...
+          </span>
+        </div>
+      </div>
+    );
+  }
   return (
     <>
       <DefaultLayoutUsers hasKTA={userData.hasKTA}>
