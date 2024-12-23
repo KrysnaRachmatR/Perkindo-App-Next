395fe936b32a3cdfc96e64e4fbeb67692da506ac
KrysnaRachmatR <krysnarachmat1@gmail.com>
2024-12-23 12:32:31 +0700

    Detail User

---
+++ src/app/admin/anggota/detailUser/page.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from "@/components/templates/DefaultAdminTemplate";
+import UserDetailTable from "@/components/organisms/admin/Detail/page";
+
+const AdminDetailAnggotaPage = () => {
+  return (
+    <DefaultLayout>
+      <UserDetailTable />
+    </DefaultLayout>
+  );
+};
+
+export default AdminDetailAnggotaPage;
