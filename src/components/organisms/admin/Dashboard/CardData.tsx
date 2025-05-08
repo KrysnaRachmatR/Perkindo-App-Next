a392523511b4b48ebfd1437e3d9bc262e5fc6c34
DmsAnhr <dimasputra162003@gmail.com>
2025-05-08 17:04:28 +0700

    dashboard update

--- src/components/organisms/admin/Dashboard/CardData.tsx
+++ src/components/organisms/admin/Dashboard/CardData.tsx
@@ -12,22 +12,24 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
   children,
 }) => {
   return (
-    <div className="rounded-md border-2 border-[#1C2434] bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
-      {/* Icon container */}
-      <div className="flex h-12 w-12 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
-        {children}
-      </div>
-
-      {/* Content section */}
-      <div className="mt-4 flex items-end justify-between">
-        <div>
-          <h4 className="text-title-md font-bold text-black dark:text-white">
-            {total}
-          </h4>
-          <span className="text-sm font-medium text-gray-500 dark:text-white">
-            {title}
-          </span>
+    <div className="rounded-xl border-2 border-gray bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark">
+      <div className="w-full flex items-center justify-between">
+        {/* Icon container */}
+        <div className="flex h-16 w-16 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
+          {children}
         </div>
+
+        <div className="mt-4 flex items-end justify-between">
+          <div>
+            <h4 className="text-title-md font-bold text-end text-black dark:text-white">
+              {total}
+            </h4>
+            <span className="text-sm font-medium text-end text-gray-500 dark:text-white">
+              {title}
+            </span>
+          </div>
+        </div>
+
       </div>
     </div>
   );
